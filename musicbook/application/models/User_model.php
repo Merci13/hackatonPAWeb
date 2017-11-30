899c8fefcb95d80cbe607fcc675015a043a9ff7f
Edwin <ejimenezu@est.utn.ac.cr>
2017-11-30 16:33:12 -0600

    Login
    Se crea el login

--- musicbook/application/models/User_model.php
+++ musicbook/application/models/User_model.php
@@ -3,7 +3,7 @@ class User_model extends CI_Model {
 
   function authenticate($user, $pass) {
 		$pass = md5($pass);
-    $query = $this->db->get_where('users',
+    $query = $this->db->get_where('musicos',
       array('correo' => $user, 'password' => $pass));
 
 	  return $query->result_object();
