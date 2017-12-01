4d5c7f9220516e0db586b600e2709f450f53c1cf
Merci13 <mrjorxe@gmail.com>
2017-11-30 23:21:46 -0600

    se elimino la hoja de controler home
    por un error en el codigo se decidio eliminar esta hora del codigo

--- musicbook/application/controllers/User.php
+++ musicbook/application/controllers/User.php
@@ -4,6 +4,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class User extends CI_Controller
 {
 
+
+   public function index(){
+
+        $this->load->view('user/home');
+    }
+
     public function login()
     {
         $this->load->view('user/login');
